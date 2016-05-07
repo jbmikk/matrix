61cfbd26deb6fe80f1b91995847ade8af2d9184b
Jens Mikkelsen <jbmikk@gmail.com>
2016-05-06 23:03:54 -0300

    Complete model matrix

--- matrix.h
+++ matrix.h
@@ -11,6 +11,7 @@ float dot_v4_v4(Vec4 *v1, Vec4 *v2);
 float dot_v4_c4(Vec4 *v, Mat4 *m, int col);
 void mul_m4_v4(Vec4 *result, Mat4 *m, Vec4 *v);
 void mul_m4(Mat4 *result, Mat4 *m1, Mat4 *m2);
+void translate_m4(Mat4 *m, float x, float y, float z);
 void scale_m4(Mat4 *m, float x, float y, float z);
 void rotate_m4(Mat4 *m, float x, float y, float z, float angle);
 
