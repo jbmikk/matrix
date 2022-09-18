579252adfbf65c7c47ae58aa9710a954993a3d30
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2022-09-18 16:36:01 -0300

    Fix Vec3 declaration

--- src/matrix.h
+++ src/matrix.h
@@ -4,7 +4,7 @@
 //#define Vec(N) float Vec##N[N]
 //#define Mat(N, M) float Mat##N##x##M[N][M]
 
-typedef float Vec3[4];
+typedef float Vec3[3];
 typedef float Vec4[4];
 
 typedef float Mat3[3][3];
@@ -17,7 +17,7 @@ void sub_v3(Vec3 *r, Vec3 *v1, Vec3 *v2);
 void scalar_v3(Vec3 *r, Vec3 *v, float s);
 void cross_v3(Vec3 *r, Vec3 *u, Vec3 *v);
 
-void init_v4(Vec3 *r, float c);
+void init_v4(Vec4 *r, float c);
 float dot_v4(Vec4 *v1, Vec4 *v2);
 float dot_v4_c4(Vec4 *v, Mat4 *m, int col);
 void mul_m4_v4(Vec4 *result, Mat4 *m, Vec4 *v);
