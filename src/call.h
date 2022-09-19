57b872889c5ee6f0f1d4c2f8cf065a4648188b1b
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2022-09-19 00:56:28 -0300

    Add type map for vectors

--- src/call.h
+++ src/call.h
@@ -1,35 +1,37 @@
 #ifndef CALL_H
 #define CALL_H
 
+#define VEC_MAP(_, JOIN, ...) \
+	_(Vec3 *, v3, __VA_ARGS__) JOIN\
+	_(Vec4 *, v4, __VA_ARGS__)
+
+#define COMMA ,
+
+#define CALL_EXPANSION(TYPE, PREFIX, NAME) TYPE: NAME##PREFIX
+
 #define v_init(V, C) \
-        _Generic((V), \
-        Vec3 *: init_v3, \
-        Vec4 *: init_v4)((V), (C))
+        _Generic((V), VEC_MAP(CALL_EXPANSION, COMMA, init_)) \
+        ((V), (C))
 
 #define v_add(R, V1, V2) \
-        _Generic((V1), \
-        Vec3 *: add_v3, \
-        Vec4 *: add_v4)((R), (V1), (V2))
+        _Generic((V1), VEC_MAP(CALL_EXPANSION, COMMA, add_)) \
+        ((R), (V1), (V2))
 
 #define v_sub(R, V1, V2) \
-        _Generic((V1), \
-        Vec3 *: sub_v3, \
-        Vec4 *: sub_v4)((R), (V1), (V2))
+        _Generic((V1), VEC_MAP(CALL_EXPANSION, COMMA, sub_)) \
+        ((R), (V1), (V2))
 
 #define v_norm(V) \
-        _Generic((V), \
-        Vec3 *: norm_v3, \
-        Vec4 *: norm_v4)((V))
+        _Generic((V), VEC_MAP(CALL_EXPANSION, COMMA, norm_)) \
+        ((V))
 
 #define v_scalar(R, V1, S) \
-        _Generic((V1), \
-        Vec3 *: scalar_v3, \
-        Vec4 *: scalar_v4)((R), (V1), (S))
+        _Generic((V1), VEC_MAP(CALL_EXPANSION, COMMA, scalar_)) \
+        ((R), (V1), (S))
 
 #define v_dot(V1, V2) \
-        _Generic((V1), \
-        Vec3 *: dot_v3, \
-        Vec4 *: dot_v4)((V1), (V2))
+        _Generic((V1), VEC_MAP(CALL_EXPANSION, COMMA, dot_)) \
+        ((V1), (V2))
 
 #endif //CALL_H
 
